18dd076f9ae1f867f462fdb1a46a95b7936803a7
Gavin Johns <gavinpjohns@gmail.com>
2016-06-27 17:18:17 -0700

    update

--- Prototype/js/archspark.js
+++ Prototype/js/archspark.js
@@ -1,1 +1,91 @@
-archspark.js
+// Structure
+// ------------------------------------------------
+var gallery = document.querySelector(".gallery");
+var dropdown = document.querySelector('.dropdown');
+var menu = document.querySelector('.menu');
+
+
+
+// Setup
+// ------------------------------------------------
+var todoList = {
+	tasks: []
+};
+
+
+// Events
+// ------------------------------------------------
+window.addEventListener("load", reloadPage);
+dropdown.addEventListener('click', showDropdown);
+menu.addEventListener('click', showMenu);
+
+
+// Event Handlers
+// ------------------------------------------------
+//rebuild the todo list with page is reloaded
+function reloadPage(e) {
+	refreshGallery();
+}
+
+
+// Update Page
+// ------------------------------------------------
+
+//creates entire todo list from json data
+function refreshGallery(){
+	//clears out the list
+	gallery.innerHTML = "";
+	//adds one new task for each item in the array
+	archspark.posts.forEach(createPost);
+}
+
+//show dropdown
+function showDropdown() {
+	dropdown.classList.toggle('dropdown-close');
+}
+//show menu
+function showMenu() {
+	menu.classList.toggle('menu-close');
+}
+
+
+//create one li on the page from a task object
+function createPost(post) {
+	console.log(post);
+
+
+
+// 1. Create Markup
+	var li = document.createElement("li");
+	var template = 
+		'<img class="image" src="' + post.image + '">' +
+		'<div class="card">' +
+			'<div class="card-header">' +
+      			'<img src="' + post.logo + '" class="logo">' +
+      				'<div class="card-title">' +
+      					'<h3 class="title">' + post.title + '</h3>' +
+						'<p class="date">' + post.date + '</p>' +
+					'</div>' +
+      		'</div>' +
+      		'<p class="summary">' + post.summary + '</p>' +
+      		'<div class="card-footer">' +
+      			'<img src="img/icons/heart.svg" class="heart">' +
+      			'<img src="img/icons/share.svg" class="share">' +
+      		'</div>' +
+      	'</div>'
+    ;
+
+
+	
+// 2. Bedazzle Markup (add attributes and content)
+	// img.setAttribute("src", post.image);
+
+
+
+// 3. Append new elements to DOM tree
+	li.innerHTML = template;
+	gallery.appendChild(li);
+}
+
+
+
---
+++ Prototype/js/content.js
@@ -0,0 +1,64 @@
+
+
+//Resources
+//--------------------------------------------
+var dezeen = {
+	id: "dezeen",
+	name: "Dezeen",
+	logo: "img/resource-logos/dezeen.png",
+	url: "http://dezeen.com/architecture"
+};
+
+var archdaily = {
+	id: "archdaily",
+	name: "ArchDaily",
+	logo: "img/resource-logos/archdaily.png",
+	url: "http://archdaily.com"
+};
+
+
+//Posts
+//--------------------------------------------
+
+var dezeen1 = {
+	resource: "dezeen",
+	image: "img/gallery/pic-1.png",
+	title: "Booya bitches",
+	date: "10 min ago",
+	summary: "Architect lorem bullnose W-section sustainable brick and mortar Miele zinc  lorem bullnose W-section sustainable brick and  lorem bullnose W-section sustainable brick and mortar Miele zinc lorem bullnose W-section sustainable brick and mortar…"
+};
+var dezeen2 = {
+	resource: "dezeen",
+	image: "img/gallery/pic-2.png",
+	title: "Module Grid House / Tetsuo Yagasaki Architects ",
+	date: "10 min ago",
+	summary: "Architect lorem bullnose W-section sustainable brick and mortar Miele zinc  lorem bullnose W-section sustainable brick and  lorem bullnose W-section sustainable brick and mortar Miele zinc lorem bullnose W-section sustainable brick and mortar…"
+};
+
+var archdaily1 = {
+	resource: "archdaily",
+	logo: "img/resource-logos/archdaily.png",
+	image: "img/gallery/pic-3.png",
+	title: "Module Grid House / Tetsuo Yagasaki Architects ",
+	date: "10 min ago",
+	summary: "Architect lorem bullnose W-section sustainable brick and mortar Miele zinc  lorem bullnose W-section sustainable brick and  lorem bullnose W-section sustainable brick and mortar Miele zinc lorem bullnose W-section sustainable brick and mortar…"
+};
+
+var archdaily2 = {
+	resource: "archdaily",
+	logo: "img/resource-logos/archdaily.png",
+	image: "img/gallery/pic-4.png",
+	title: "Module Grid House / Tetsuo Yagasaki Architects ",
+	date: "10 min ago",
+	summary: "Architect lorem bullnose W-section sustainable brick and mortar Miele zinc  lorem bullnose W-section sustainable brick and  lorem bullnose W-section sustainable brick and mortar Miele zinc lorem bullnose W-section sustainable brick and mortar…"
+};
+
+
+
+//All app data
+//--------------------------------------------
+var archspark = {
+	resources: [ archdaily, dezeen ],
+	posts: [ dezeen1, archdaily1, dezeen2, archdaily2, dezeen2, archdaily1, dezeen1, dezeen2,  dezeen1, archdaily1, dezeen2, archdaily2, dezeen2, archdaily1, dezeen1, dezeen2,  dezeen1, archdaily1, dezeen2, archdaily2, dezeen2, archdaily1, dezeen1, dezeen2, dezeen1, archdaily1, dezeen2, archdaily2, dezeen2, archdaily1, dezeen1, dezeen2, ]
+};
+
